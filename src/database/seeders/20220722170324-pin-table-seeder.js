1e29b4e93ebf43c2fd53d5849528cf67b84553bb
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-07-24 21:06:44 +0630

    updated sample data

--- src/database/seeders/20220722170324-pin-table-seeder.js
+++ src/database/seeders/20220722170324-pin-table-seeder.js
@@ -2,26 +2,70 @@
 
 const _ = require('lodash');
 
-const geolocations = [
-  { long: 1.3629, lang: 103.75336 },
-  { long: 1.3644, lang: 103.75259 },
-  { long: 1.3642, lang: 103.74962 },
-  { long: 1.36242, lang: 103.7481 },
-  { long: 1.36664, lang: 103.74923 },
-  { long: 1.37054, lang: 103.75274 },
-  { long: 1.37062, lang: 103.75579 },
-  { long: 1.36628, lang: 103.75829 },
-  { long: 1.36332, lang: 103.75623 },
-  { long: 1.3636, lang: 103.756 },
-  { long: 1.3629, lang: 103.7501 },
+const initialData = [
+  {
+    long: 1.3629,
+    lang: 103.75336,
+    image: 'https://loremflickr.com/g/320/240/factory',
+  },
+  {
+    long: 1.3644,
+    lang: 103.75259,
+    image: 'https://loremflickr.com/g/320/240/paris',
+  },
+  {
+    long: 1.3642,
+    lang: 103.74962,
+    image: 'https://loremflickr.com/g/320/240/building',
+  },
+  {
+    long: 1.36242,
+    lang: 103.7481,
+    image: 'https://loremflickr.com/g/320/240/cars',
+  },
+  {
+    long: 1.36664,
+    lang: 103.74923,
+    image: 'https://loremflickr.com/g/320/240/people',
+  },
+  {
+    long: 1.37054,
+    lang: 103.75274,
+    image: 'https://loremflickr.com/g/320/240/cat',
+  },
+  {
+    long: 1.37062,
+    lang: 103.75579,
+    image: 'https://loremflickr.com/g/320/240/dog',
+  },
+  {
+    long: 1.36628,
+    lang: 103.75829,
+    image: 'https://loremflickr.com/g/320/240/code',
+  },
+  {
+    long: 1.36332,
+    lang: 103.75623,
+    image: 'https://loremflickr.com/g/320/240/success',
+  },
+  {
+    long: 1.3636,
+    lang: 103.756,
+    image: 'https://loremflickr.com/g/320/240/football',
+  },
+  {
+    long: 1.3629,
+    lang: 103.7501,
+    image: 'https://loremflickr.com/g/320/240/golf',
+  },
 ];
-const pins = geolocations.map((location, index) => ({
+const pins = initialData.map((d, index) => ({
   name: `Camera-${index + 1}`,
-  long: location.long,
-  lang: location.lang,
+  long: d.long,
+  lang: d.lang,
   remarks: 'this is remark',
   is_alert: _.random(1, 2) === 1 ? true : false,
-  image: '',
+  image: d.image,
   department_id: _.random(1, 3),
   group_id: _.random(1, 3),
   created_at: new Date(),
