caca1ad51246653862de8024bc03a7b5b6c20152
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-07-24 21:14:25 +0630

    updated db configs

--- src/config/config.js
+++ src/config/config.js
@@ -9,5 +9,11 @@ module.exports = {
     dialect: 'mysql',
   },
   test: {},
-  production: {},
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
 };
